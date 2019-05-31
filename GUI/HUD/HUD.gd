5cf09a55387ac23230a257132b163211e35fb292
Dima Goncharov <dima.goncharov@hotmail.com>
2019-05-31 17:45:11 -0400

    upgrade player HUD

--- GUI/HUD/HUD.gd
+++ GUI/HUD/HUD.gd
@@ -16,23 +16,52 @@ func _ready():
 	gameState.connect("update_player_skrilla", self, "_onUpdatePlayerSkrilla")
 	gameState.connect("update_ars_health", self, "_onUpdateARSHealth")
 	gameState.connect("update_player_score", self, "_onUpdatePlayerScore")
+	$ARSHealth/ARSHealthBar.max_value = gameState.arsMaxHealth
+	$PlayerHealth/PlayerHealthBar.max_value = gameState.playerMaxHealth
 	
 
 func _onUpdatePlayerMessage(message: String):
 	$PlayerMessage.text = message
 	
 func _onUpdatePlayerHealth(playerHealth: int):
-	$PlayerHealth.text = PLAYER_HEALTH_LABEL + SEPARATOR + String(playerHealth)
+	$PlayerHealth/PlayerHealthBar.value = playerHealth
+	if playerHealth != gameState.playerMaxHealth:
+		flashPlayerText()
 	
 func _onUpdateARSHealth(arsHealth: int):
-	$ARSHealth.text = ARS_HEALTH_LABEL + SEPARATOR + String(arsHealth)
+	$ARSHealth/ARSHealthBar.value = arsHealth
+	if arsHealth != gameState.arsMaxHealth :
+		flashARSText()
 	
 func _onUpdatePlayerSkrilla(playerSkrilla: int):
 	$PlayerSkrilla.text = SKRILLA_LABEL + SEPARATOR + String(playerSkrilla)
+	flashSkrillaText()
 	
 func _onUpdatePlayerScore(playerScore: int):
 	$PlayerScore.text = SCORE_LABEL + SEPARATOR + String(playerScore)
+	flashScoreText()
+	
+func flashPlayerText() -> void:
+	$PlayerHealth/PlayerText/AnimationPlayer.play("FlashRed")
+	yield(get_tree().create_timer(1), "timeout")
+	$PlayerHealth/PlayerText/AnimationPlayer.stop()
+
+func flashARSText() -> void:
+	$ARSHealth/ARSText/AnimationPlayer.play("FlashRed")
+	yield(get_tree().create_timer(1), "timeout")
+	$ARSHealth/ARSText/AnimationPlayer.stop()
+	
+func flashScoreText() -> void:
+	$PlayerScore/AnimationPlayer.play("FlashGreen")
+	yield(get_tree().create_timer(1), "timeout")
+	$PlayerScore/AnimationPlayer.stop()
+	
+func flashSkrillaText() -> void:
+	$PlayerSkrilla/AnimationPlayer.play("FlashGreen")
+	yield(get_tree().create_timer(1), "timeout")
+	$PlayerSkrilla/AnimationPlayer.stop()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	$ARSHealth/ARSHealthBar.max_value = gameState.arsMaxHealth
+	$PlayerHealth/PlayerHealthBar.max_value = gameState.playerMaxHealth
